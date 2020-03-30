5ce25d4e868cda0a093bf594d9b40681c58106a9
Vincent Guénin <guenin.vincent4@gmail.com>
2020-03-30 13:13:42 +0200

    Add random for two pokemons at the same speed

--- src/main/TurnOrder.ts
+++ src/main/TurnOrder.ts
@@ -1,8 +1,17 @@
 import {Pokemon} from "./Pokemon";
 
+function getRandomInt(max: number): number {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
 function turn_order(p1 : Pokemon, p2 : Pokemon) : Pokemon {
     let cheater = xor_cheater(p1, p2);
     if(cheater !== null) return cheater;
+
+    if(p1.speed === p2.speed) {
+        if(getRandomInt(1) == 0) { return p1; }
+        else { return p2; }
+    }
 
     return p1.speed > p2.speed ? p1 : p2;
 }
